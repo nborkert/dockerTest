e3a58509699fbcb15b18363d1d7f96f1914c9783
Neal Borkert <ndborkedaunt@neal-borkerts-imac-3.local>
2014-04-24 20:11:37 -0400

    removed extraneous echo statements

--- QA_pullrun.sh
+++ QA_pullrun.sh
@@ -4,8 +4,6 @@ docker pull nborkert/$1
 WC=`docker run --cidfile="$1.cid" $1`
 
 CID=`cat $1.cid`
-echo 'running'
-echo $CID
 
 if [ $WC > "0" ]
 then
--- pipeline.sh
+++ pipeline.sh
@@ -4,13 +4,10 @@ docker build -t $1 - < firstTest
 WC=`docker run --cidfile="$1.cid" piptest`
 
 CID=`cat $1.cid`
-echo 'running'
-echo $CID
 
 if [ $WC > "0" ]
 then
   # echo 'SUCCESS' 
-  #now get the container id of what was just run
   docker commit $CID nborkert/$1
   docker push nborkert/$1
 fi
