5f508ecaca741e181314e1760ddfa85ea733ab79
Neal Borkert <ndborkedaunt@neal-borkerts-imac-3.local>
2014-06-14 14:18:29 -0400

    Script tests adding local file instead of remote URL content for test word

---
+++ localFileTestPipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build -t $1 .
+WC=`docker run --cidfile="$1.cid" $1`
+
+CID=`cat $1.cid`
+
+if [ $WC -gt 0 ]
+then
+  echo 'SUCCESS' 
+  docker commit $CID nborkert/$1
+  docker push nborkert/$1
+fi
+
+rm $1.cid
+# rm 0
+
