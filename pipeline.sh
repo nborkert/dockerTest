f41d67c556a5c52e2b0565db1bcf494e5bcbcfaa
Neal Borkert <ndborkedaunt@neal-borkerts-imac-3.local>
2014-06-14 13:17:48 -0400

    fixed logic error that was always returning true

--- pipeline.sh
+++ pipeline.sh
@@ -5,13 +5,13 @@ WC=`docker run --cidfile="$1.cid" $1`
 
 CID=`cat $1.cid`
 
-if [ $WC > "0" ]
+if [ $WC -gt 0 ]
 then
-  # echo 'SUCCESS' 
+ # echo 'SUCCESS' 
   docker commit $CID nborkert/$1
   docker push nborkert/$1
 fi
 
 rm $1.cid
-rm 0
+# rm 0
 
